1194400657a466c913253b94250b6b8542cc9396
Jorge Saravia <Jorge.Saravia@tegraglobal.com>
2022-05-13 14:18:49 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(props) {
+  console.log(props);
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  )
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around.</p>
+      <ul style={{ textAlign: "left" }}>
+        {props.dishes.map((dish) => <li>{dish}</li>)}
+      </ul>
+    </section>
+  )
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  )
+}
+
+const dishes = [
+  "Macaroni and Cheese",
+  "Salmon",
+  "Tofu with Vegetables"
+];
+
+dishes.map((dish) => console.log(dish))
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Jorge" />
+      <Main adjective="amazing" dishes={dishes} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
